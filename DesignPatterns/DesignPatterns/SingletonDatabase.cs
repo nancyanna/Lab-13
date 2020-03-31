74a73038bbc98ca42c6a9faa2cc95d508750caa8
nancyanna <61117785+nancyanna@users.noreply.github.com>
2020-03-31 11:57:13 -0500

    Add files via upload

---
+++ DesignPatterns/DesignPatterns/SingletonDatabase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns
+{
+    class SingletonDatabase
+    {
+        private static SingletonDatabase _instance = null;
+        public static SingletonDatabase Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new SingletonDatabase();
+                }
+                return _instance;
+
+            }
+        }
+        private SingletonDatabase()
+        {
+
+        }
+    }
+}
